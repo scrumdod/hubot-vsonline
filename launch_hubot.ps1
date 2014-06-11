a7f4fc71c39369ed2510ede4fcdf6e1616c1e9dc
Rob Maher <scrumdod@gmail.com>
2014-06-11 07:09:28 -0400

    initial commit

---
+++ launch_hubot.ps1
@@ -0,0 +1,13 @@
+﻿$env:HUBOT_ACCOUNT_NAME="tfsbot"
+$env:HUBOT_ACCOUNT_PWD="Diesel12"
+$env:HUBOT_PROJECT_NAME="Git1"
+$env:HUBOT_TFID="99e52418-128b-4391-8f68-c74466dddfa1"
+$env:HUBOT_TFSERVICE_NAME="https://hubottest.vsoalm.tfsallin.net/"
+$env:HUBOT_ROOMS="HubotRoom Team Room"
+$env:PORT="8080"
+$env:HUBOT_COLLECTION_NAME = "DefaultCollection"
+
+cd\
+cd users\rob\hubotworking\node_modules\hubot
+node .\node_modules\coffee-script\bin\coffee .\bin\hubot -a vsOnline
+
